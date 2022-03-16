ecf83cd9e4544b7b5619c1173a0fd15dbc0e6975
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-16 21:19:51 +0700

    #Commit 16/03/2022: Add footer and slider-bar

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CommonsRoutingModule } from './commons-routing.module';
 import { CommonHeaderComponent } from './common-header/common-header.component';
+import { CommonFooterComponent } from './common-footer/common-footer.component';
 
 
 @NgModule({
   declarations: [
-    CommonHeaderComponent
+    CommonHeaderComponent,
+    CommonFooterComponent
   ],
   imports: [
     CommonModule,
     CommonsRoutingModule
   ],
   exports: [
-    CommonHeaderComponent
+    CommonHeaderComponent,
+    CommonFooterComponent
   ]
 })
 export class CommonsModule { }
